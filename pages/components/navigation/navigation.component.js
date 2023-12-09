c014f09c0287f6d891ed0252f802a92a7d58fa4b
[Kelvin Gothman] <[kmgothman@gmail.com]>
2023-12-09 15:19:22 -0500

    style changes to nav

--- pages/components/navigation/navigation.component.js
+++ pages/components/navigation/navigation.component.js
@@ -1,10 +1,30 @@
+'use client'
+
 import styles from './navigation.module.css'
+import { useState, useEffect } from 'react';
 
 const Navigation = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+          const scrollTop = window.scrollY;
+          setIsScrolled(scrollTop > 0);
+        };
+    
+        // Add scroll event listener
+        window.addEventListener('scroll', handleScroll);
+    
+        // Clean up the event listener on component unmount
+        return () => {
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []); // Empty dependency array ensures the effect runs only once
+    
 
     return (
-        <div className={styles.nav}>
-            <img src='/images/logo.png' width='150px' />
+        <div className={`${styles.nav} ${isScrolled ? styles.colorBackground : ''}`} >
+            <img src='/images/logo.png' width={isScrolled ? '110' : '150'} />
             <div>
                 <a>About</a>
                 <a>Episodes</a>
